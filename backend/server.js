69c2395d486e0e59c7925fb7ce9dccc998b96f78
Mehul Gala <mehulgala007@gmail.com>
2025-01-25 01:28:06 +0530

    Added Error Middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
-import products from "./data/products.js";
 import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -11,18 +12,10 @@ connectDB();
 
 const app = express();
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
-
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
