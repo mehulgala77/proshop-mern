8ee376f1b42ed678b174513a1a9ef865973d32cc
Mehul Gala <mehulgala007@gmail.com>
2025-01-27 13:30:43 +0530

    Added user controller and routes and implemented basic auth route logic

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import connectDB from "./config/db.js";
+
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import dotenv from "dotenv";
@@ -12,7 +15,11 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
