fd2a309cb4b8742046b61b53dd3c75cbaa9b7cd9
Mehul Gala <mehulgala007@gmail.com>
2025-01-27 20:29:01 +0530

    Implemented Login form, Auth Slice and Users API Slice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -15,6 +18,7 @@ const App = () => {
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
   );
 }
