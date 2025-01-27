68d45be250f42b4017d50c9c66c29c38eff4fa4d
Mehul Gala <mehulgala007@gmail.com>
2025-01-27 14:14:39 +0530

    Added Logout user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -47,7 +47,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  // Note: Expire the HTTP Only Cookie
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 // @desc    Get user profile
