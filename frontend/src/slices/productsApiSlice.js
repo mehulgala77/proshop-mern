8ddbd1a26c1057637c1efcabb518a4b12858f5df
Mehul Gala <mehulgala007@gmail.com>
2025-02-03 02:01:16 +0530

    Implemented Pagination for products

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -5,8 +5,9 @@ import { apiSlice } from "./apiSlice";
 export const productSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getProducts: builder.query({
-      query: () => ({
+      query: ({ pageNumber }) => ({
         url: PRODUCTS_URL,
+        params: { pageNumber },
       }),
       providesTags: ["Products"],
       keepUnusedDataFor: 5,
