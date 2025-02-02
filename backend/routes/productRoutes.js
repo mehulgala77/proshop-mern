2032713376f0aa7eed5f7f4a1ad67f7eec426473
Mehul Gala <mehulgala007@gmail.com>
2025-02-02 12:03:37 +0530

    Added Create New Sample Product Feature

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,9 +4,12 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  createProduct,
 } from "../controllers/productController.js";
 
-router.route("/").get(getProducts);
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.route("/:id").get(getProductById);
 
 export default router;
